b6e18040154f35cc8cdcf825e88a0825f9912406
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-29 15:41:14 +0800

    to get Uppercase started name in Jason data.

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -15,7 +16,14 @@ namespace CityInfo.API
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+      services.AddMvc()
+        .AddJsonOptions(o =>
+        {
+          if (o.SerializerSettings.ContractResolver is DefaultContractResolver castedResolver)
+          {
+            castedResolver.NamingStrategy = null;
+          }
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
