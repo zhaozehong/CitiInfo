bcbe210c400987a7b8dafb2a9604715c17b0162e
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-29 16:22:34 +0800

    support returning XML data format

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -17,13 +18,15 @@ namespace CityInfo.API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc()
-        .AddJsonOptions(o =>
-        {
-          if (o.SerializerSettings.ContractResolver is DefaultContractResolver castedResolver)
-          {
-            castedResolver.NamingStrategy = null;
-          }
-        });
+        .AddMvcOptions(o => o.OutputFormatters.Add(
+          new XmlDataContractSerializerOutputFormatter()));
+      //.AddJsonOptions(o =>
+      //{
+      //  if (o.SerializerSettings.ContractResolver is DefaultContractResolver castedResolver)
+      //  {
+      //    castedResolver.NamingStrategy = null;
+      //  }
+      //});
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
