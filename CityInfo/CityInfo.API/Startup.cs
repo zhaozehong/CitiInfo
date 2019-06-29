92f29018a6ffe58257b109595a86b43068e13009
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-29 11:45:15 +0800

    add a middleware in Configurate method

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -24,11 +24,20 @@ namespace CityInfo.API
       {
         app.UseDeveloperExceptionPage();
       }
-
-      app.Run(async (context) =>
+      else
       {
-        await context.Response.WriteAsync("Hello World!");
+        app.UseExceptionHandler();
+      }
+
+      app.Run((context) =>
+      {
+        throw new Exception("something bad happened");
       });
+
+      //app.Run(async (context) =>
+      //{
+      //  await context.Response.WriteAsync("Hello World!");
+      //});
     }
   }
 }
