dfefc09ef25bc2b0385d7982cd70414d91e212d7
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-30 14:23:42 +0800

    Detele a data with DELETE method

--- CityInfo/CityInfo.API/Controllers/PointOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointOfInterestController.cs
@@ -123,5 +123,20 @@ namespace CityInfo.API.Controllers
 
       return NoContent(); // as the consumer already has all the information
     }
+    [HttpDelete("{cityId}/pointsofinterest/{id}")]
+    public IActionResult DeletePointOfInterest(int cityId, int id)
+    {
+      var city = CitiesDataStore.Current.GetCity(cityId);
+      if (city == null)
+        return NotFound();
+
+      var point = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+      if (point == null)
+        return NotFound();
+
+      city.PointsOfInterest.Remove(point);
+
+      return NoContent();
+    }
   }
 }
